6a964b17aab09d4e1e304f1bcc1ee0b9d75e49c6
1nsafo <evgeniy10200@mail.ru>
2025-04-09 22:56:04 +0300

    Prepare the data

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -1,5 +1,10 @@
-package main
+package models
 
-func main(){
-	
+type Film struct {
+    ID          int     `json:"id"`           
+    Title       string  `json:"title"`        
+    ReleaseYear string     `json:"release_year"` 
+    Director    string  `json:"director"`     
+    Genre       string  `json:"genre"`        
+    Rating      float64 `json:"rating"`       
 }
