99db0353d307d4b29a8e196bd42833c7f9608d34
1nsafo <evgeniy10200@mail.ru>
2025-04-10 23:49:17 +0300

    Prepare the date 2

--- cmd/http-server/service1.go
+++ cmd/http-server/service1.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"Service/internal/models"
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
-)
 
 func main() {
-	byteValue, err := os.ReadFile("data/movies_dataset.json")
-	if err != nil {
-		log.Fatalf("Ошибка чтения файла: %v", err)
-	}
-
-	var films []models.Film
-	err = json.Unmarshal(byteValue, &films)
-	if err != nil {
-		log.Fatalf("Ошибка разбора JSON: %v", err)
-	}
-
-	for _, film := range films {
-		fmt.Printf("ID: %d, Title: %s, ReleaseYear: %s, Director: %s, Genre: %s, Rating: %1f\n",
-			film.ID, film.Title, film.ReleaseYear, film.Director, film.Genre, film.Rating)
-
-	}
 }
